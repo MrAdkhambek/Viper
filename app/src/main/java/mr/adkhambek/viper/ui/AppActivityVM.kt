4f93da0427ec56846a44148a03015b638223a1cb
Adkhambek <mr.adkhambek@gmail.com>
2022-01-20 06:21:59 +0500

    migrate cicerone

--- app/src/main/java/mr/adkhambek/viper/ui/AppActivityVM.kt
+++ app/src/main/java/mr/adkhambek/viper/ui/AppActivityVM.kt
@@ -2,14 +2,7 @@ package mr.adkhambek.viper.ui
 
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 @HiltViewModel
-class AppActivityVM @Inject constructor(
-    private val navigationDispatcher: NavigationDispatcher
-) : ViewModel() {
-
-    val navigationCommands: Flow<NavigationCommand>
-        get() = navigationDispatcher.navigationCommands
-}
+class AppActivityVM @Inject constructor() : ViewModel()
